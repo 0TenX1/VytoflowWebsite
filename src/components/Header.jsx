617a1efd2bc71dd25a223474d0403aa58b292ef6
suraj060203 <suraj06022003@gmail.com>
2024-05-20 23:56:41 +0530

    made the navbar
    replaced the old code to make the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { vytoflowLogo } from "../assets";
+import './Header.css'
 
 const Header = () => {
   return (
-    <div className="fixed top-0 z-50 bg-neutral-50 backdrop-blur-sm border-b border-neutral-600 lg:bg-neutral-50 lg:backdrop-blur-sm">
-      <div className="flex items-center px-1 lg:px-5 xl:px-5 max-lg:py-4">
-        <a className="block w-[10rem] lg:-mr-5" href="#hero">
-          <img src={vytoflowLogo} width={190} height={80} alt="Vytoflowtech" />
-        </a>
-      </div>
+    <div>
+      <div className="navbar">
+        <img src={vytoflowLogo} alt="VytoFlow Logo" className="logo"/>
+        <div className="heading" id="home">Home</div>
+        <div className="heading" id="home">Explore</div>
+        <div className="heading" id="home">About</div>
+        <div className="heading" id="home">Contact</div>
+      </div> <hr/>
     </div>
   );
 };
