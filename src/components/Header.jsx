4d0b8fa3946b2390c3a67f9b6af96e3b130c80bd
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-05-21 00:48:18 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,35 @@
 import { vytoflowLogo } from "../assets";
-import './Header.css'
+import { navigation } from "../constants";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const pathname = useLocation();
   return (
-    <div>
-      <div className="navbar">
-        <img src={vytoflowLogo} alt="VytoFlow Logo" className="logo"/>
-        <div className="heading" id="home">Home</div>
-        <div className="heading" id="home">Explore</div>
-        <div className="heading" id="home">About</div>
-        <div className="heading" id="home">Contact</div>
-      </div> <hr/>
+    <div className="fixed top-0 z-50 w-full bg-n-8/90 backdrop-blur-sm border-b-2 border-b-cyan-800 lg:bg-white lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-5 xl:px-10 max-lg:py-4">
+        <a className="block w-[10rem] xl:mr-8" href="#hero">
+          <img src={vytoflowLogo} width={190} height={80} alt="Vytoflowtech" />
+        </a>
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:ml-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row lg:justify-end">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-cyan-800 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                }px-6 py-6 md:py-8 lg:-mr-0.25 text-sm lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-cyan-600"
+                    : "lg:text-n-4"
+                }`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#home",
+  },
+  {
+    id: "1",
+    title: "Explore",
+    url: "#explore",
+  },
+  {
+    id: "2",
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: "3",
+    title: "Contact",
+    url: "#contact",
+  },
+];
