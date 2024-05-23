503a365ba1228f353248d8c66042a80a49eedc63
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-05-23 08:43:54 +0530

    working on parallax Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import "tailwindcss/tailwind.css";
 
 const Hero = () => {
   return (
-    <section className="flex h-screen relative overflow-hidden items-center justify-center bg-blue-200">
+    <section className="flex h-screen relative overflow-hidden items-center justify-center bg-blue-100">
       <div className=" relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">Connect the virtual</h1>
