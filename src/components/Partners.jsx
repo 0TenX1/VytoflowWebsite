3f34e90fbc9bfe99552556579d6a58052e77f896
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-05-30 20:23:35 +0530

    Partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { agratas, cetpa, gitHub, internshala, microsoft, samsung } from "../assets";
 import { abesit, akhilesh, dme, drona, gcet, gniot, iimt, its, km, mewar, srm, zk, akgec, glbajaj, iec, ims, niet, sharda, abesec } from "../assets";
 
@@ -11,6 +12,17 @@ const Partners = () => {
 
   const Company = [ microsoft, samsung , gitHub, internshala,agratas,cetpa ] ;
 
+  const itemVariants = {
+    initial: { scale: 1 },
+    hover: { scale: 1.1 },
+  };
+
+  const spring = {
+    type: "spring",
+    stiffness: 500,
+    damping: 30
+  };
+
   return (
     <div className="relative h-auto w-full bg-gray-800 p-8">
       <h2 className="text-center text-5xl mt-4 text-white font-bold mb-8 ">
@@ -18,9 +30,15 @@ const Partners = () => {
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-6">
         {Colleges.map((image, index) => (
-          <div key={index} className="flex justify-center items-center">
+          <motion.div 
+            key={index} 
+            className="flex justify-center items-center"
+            whileHover="hover"
+            variants={itemVariants}
+            transition={spring}
+          >
             <img src={image} alt={`Partner College ${index + 1}`} className="h-52 w-52 object-contain" />
-          </div>
+          </motion.div>
         ))}
       </div>
       <h2 className="text-center lg:mt-9 text-5xl text-white font-bold mb-8 ">
@@ -28,9 +46,15 @@ const Partners = () => {
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-6">
         {Company.map((image, index) => (
-          <div key={index} className="flex justify-center items-center">
-            <img src={image} alt={`Partner College ${index + 1}`} className="h-52 w-52 object-contain" />
-          </div>
+          <motion.div 
+            key={index} 
+            className="flex justify-center items-center"
+            whileHover="hover"
+            variants={itemVariants}
+            transition={spring}
+          >
+            <img src={image} alt={`Partner Company ${index + 1}`} className="h-52 w-52 object-contain" />
+          </motion.div>
         ))}
       </div>
     </div>
