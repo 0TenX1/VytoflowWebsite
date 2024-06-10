5953a5726e398d279376d18247f77f73e0c17d2a
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-06-11 00:24:09 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,24 +17,27 @@ const Hero = () => {
   const buildingTwoY = useTransform(scrollY, [0, 100], [0, 10]);
   const buildingThreeY = useTransform(scrollY, [0, 300], [0, 100]);
 
-  return (
-    <section className="flex h-auto bg-gradient-to-b from-blue-200 via-blue-200 to-white relative overflow-hidden items-center justify-center">
-      <motion.div className="relative mx-auto text-center " style={{ y: textY }}>
-      <h1 className="relative opacity-100 text-6xl top-40 md:text-8xl z-10">VytoFlow Tech</h1>
-      <h2 className="relative py-20 md:py-6 text-2xl top-24 md:top-44 lg:top-44 md:text-4xl z-10">Connect The Virtual</h2>
+ 
 
+  return (
+    
+    <div className="flex h-auto bg-gradient-to-b from-blue-200 via-blue-200 to-white relative overflow-hidden items-center justify-center">
+      <motion.div className="relative mx-auto text-center " style={{ y: textY }}>
+      <h1 className="relative opacity-100 text-6xl top-40 md:text-8xl stroke-text russo-one-font z-10">VytoFlow Tech</h1>
+      <h2 className="relative py-20 md:py-6 text-3xl top-24 md:top-44 lg:top-44 stroke-text font-bold saira-font md:text-6xl z-10">Connect The Virtual</h2>
+     
           <div className="relative md:top-[17rem] top-[6rem] lg:top-[27rem]">
-          <div style={{ zIndex: 50 }} className="absolute md:-left-32 -left-32 right-1/3  bottom-1/3 text-md md:text-2xl lg:text-3xl text-center">
+          <div style={{ zIndex: 50 }} className="absolute bebas-neue-regular md:-left-32 -left-32 right-1/3  bottom-1/3 text-md md:text-2xl lg:text-4xl text-center">
             <h2>Collaborators:</h2>
             <CountUp end={50} duration={5} />
             +
             </div>
-            <div style={{ zIndex: 50 }} className="absolute md:left-0 md:right-0 left-0 right-0 lg:left-0 lg:right-0 bottom-1/3 text-md md:text-2xl lg:text-3xl text-center">
+            <div style={{ zIndex: 50 }} className="absolute bebas-neue-regular md:left-0 md:right-0 left-0 right-0 lg:left-0 lg:right-0 bottom-1/3 text-md md:text-2xl lg:text-4xl text-center">
             <h2>Projects:</h2>
             <CountUp end={60} duration={5} />
             +
             </div>
-            <div style={{ zIndex: 50 }} className="absolute md:-right-32 -right-32 left-1/3  bottom-1/3 text-md md:text-2xl lg:text-3xl text-center">
+            <div style={{ zIndex: 50 }} className="absolute bebas-neue-regular md:-right-32 -right-32 left-1/3  bottom-1/3 text-md md:text-2xl lg:text-4xl text-center">
             <h2>Trusted Members:</h2>
             <CountUp end={2000} duration={5} />
             +
@@ -74,7 +77,41 @@ const Hero = () => {
           style={{ y: buildingThreeY }}
         />
       </motion.div>
-    </section>
+      <style>
+    {`
+   /* Import the fonts */
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Serif+Text:ital@0;1&family=Fjalla+One&family=Russo+One&family=Saira:ital,wght@0,100..900;1,100..900&display=swap');
+
+.vytoflow-font {
+  font-family: 'DM Serif Text', serif;
+}
+
+.russo-one-font {
+  font-family: 'Russo One', sans-serif;
+}
+
+.saira-font {
+  font-family: 'Saira', sans-serif;
+}
+.bebas-neue-regular {
+  font-family: "Bebas Neue", sans-serif;
+  font-weight: 200;
+  font-style: normal;
+}
+.stroke-text {
+
+  color: white; /* Text color */
+  -webkit-text-stroke: 0.5px #023e8a; /* Stroke size and color */
+  text-shadow: 
+    -1px -1px 0 black,  
+    1px -1px 0 black,
+    -1px 1px 0 black,
+    1px 1px 0 black; /* Shadow directions and color */
+}
+
+    `}
+  </style>
+</div>
   );
 };
 
