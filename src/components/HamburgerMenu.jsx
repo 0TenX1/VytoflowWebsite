e14bbf5ce87339ca2d8f99d381845b2f932ce9b2
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-06-11 02:20:44 +0530

    changes

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -18,10 +18,10 @@ const HamburgerMenu = () => {
       </button>
       <nav className={`menu ${isOpen ? 'open' : ''}`}>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#explore">Explore</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact Us</a></li>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/community">Community</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+          <li><a href="/event">Coming Soon</a></li>
           {/* <li><a href="#community">Community</a></li> */}
         </ul>
       </nav>
