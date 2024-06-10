d7e39d46caf90552150d5c3341c811fcf3b39e24
Siddhant <118176797+0TenX1@users.noreply.github.com>
2024-06-10 22:26:11 +0530

    changes

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -53,8 +53,10 @@ const ImageContainer2 = styled.div`
   justify-content: center;
   align-items: center;
   cursor: pointer;
-  margin-left: 50px;
-  border-radius: 10px
+  margin-left: 20px;
+  margin-top: 10px;
+  border-radius: 10px;
+
 `;
 
 const Image2 = styled(motion.img)`
@@ -73,107 +75,111 @@ const Community = () => {
     <Container className="h-auto w-auto bg-white flex flex-col justify-start items-center">
       {/* Row of 5 white boxes */}
       <div className="relative top-28 text-n-5 text-7xl">Previous Events Glimpses</div>
-      <div className="relative top-32 text-n-5 mt-10  text-4xl">Programming Quiz Competition</div>
-      <div className="flex justify-between w-full px-8 mt-52">
-        {/* Image 1 */}
-        <ImageContainer>
-          <Image src={pqOne} alt="Image 1" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+      <div className="relative top-32 text-n-5 mt-10 left-10 text-4xl">Programming Quiz Competition</div>
+      <div className="flex justify-between flex-wrap lg:px-8 px-20 mt-52">
+    {/* Image 1 */}
+    <ImageContainer className="w-full mr-2 mb-2 lg:w-full">
+        <Image src={pqOne} alt="Image 1" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
 
-        {/* Image 2 */}
-        <ImageContainer>
-          <Image src={pqTwo} alt="Image 2"whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+    {/* Image 2 */}
+    <ImageContainer className="w-full mr-2 mb-2 lg:w-full">
+        <Image src={pqTwo} alt="Image 2" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
 
-        {/* Image 3 */}
-        <ImageContainer>
-          <Image src={ppBanner2024} alt="Image 3" whileHover={{ scale: 1.8 }} />
-        </ImageContainer>
+    {/* Image 3 */}
+    <ImageContainer className="w-full mr-2 mb-2 sm:w-1/2 lg:w-full">
+        <Image src={ppBanner2024} alt="Image 3" whileHover={{ scale: 1.8 }} />
+    </ImageContainer>
 
-        {/* Image 4 */}
-        <ImageContainer>
-          <Image src={pqThree} alt="Image 4" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+    {/* Image 4 */}
+    <ImageContainer className="w-full mr-2 mb-2 sm:w-1/2 lg:w-full">
+        <Image src={pqThree} alt="Image 4" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
 
-        {/* Image 5 */}
-        <ImageContainer>
-          <Image src={pqFour} alt="Image 5" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
-      </div>
-      <div className="relative top-32 text-n-5 -mt-10 text-4xl">Group Discussion Competition</div>
-      <div className="flex justify-between w-full px-8 mt-52">
-        {/* Image 1 */}
-        <ImageContainer>
-          <Image src={gdOne} alt="Image 1" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+    {/* Image 5 */}
+    <ImageContainer className="w-full sm:w-1/2 lg:w-full">
+        <Image src={pqFour} alt="Image 5" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
+</div>
 
-        {/* Image 2 */}
-        <ImageContainer>
-          <Image src={gdTwo} alt="Image 2"whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+      <div className="relative top-32 text-n-5 left-10 -mt-10 text-4xl">Group Discussion Competition</div>
+      <div className="flex justify-between flex-wrap lg:px-8 px-20 mt-52">
+    {/* Image 1 */}
+    <ImageContainer className="w-full sm:w-auto mb-2 mr-2  sm:mb-0">
+        <Image src={gdOne} alt="Image 1" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
 
-        {/* Image 3 */}
-        <ImageContainer>
-          <Image src={gdBanner2023} alt="Image 3" whileHover={{ scale: 2 }} />
-        </ImageContainer>
+    {/* Image 2 */}
+    <ImageContainer className="w-full sm:w-auto mb-2 mr-2 sm:mb-0">
+        <Image src={gdTwo} alt="Image 2" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
 
-        {/* Image 4 */}
-        <ImageContainer>
-          <Image src={gdThree} alt="Image 4" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
+    {/* Image 3 */}
+    <ImageContainer className="w-full sm:w-auto mb-2 mr-2 sm:mb-0">
+        <Image src={gdBanner2023} alt="Image 3" whileHover={{ scale: 2 }} />
+    </ImageContainer>
 
-        {/* Image 5 */}
-        <ImageContainer>
-          <Image src={pqFour} alt="Image 5" whileHover={{ scale: 1.3 }} />
-        </ImageContainer>
-      </div>
+    {/* Image 4 */}
+    <ImageContainer className="w-full sm:w-auto mb-2 mr-2 sm:mb-0">
+        <Image src={gdThree} alt="Image 4" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
+
+    {/* Image 5 */}
+    <ImageContainer className="w-full sm:w-auto mb-2 mr-2 sm:mb-0">
+        <Image src={pqFour} alt="Image 5" whileHover={{ scale: 1.3 }} />
+    </ImageContainer>
+</div>
+
 
       {/* Founder section */}
-      <div className="relative top-16 right-0 mb-8 text-n-5 text-6xl">Founder:</div>
-      <div className="flex flex-row mt-24 ml-8">
-        <div className="flex flex-col items-center">
-          <img src={ansh} alt="Founder" className="w-56 border-4 border-slate-300 h-full" />
-          <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 text-black border-2 border-cyan-950 text-lg flex items-center">
+      <div className="relative top-16 right-0 mb-8 text-n-5 text-6xl text-center">Founder:</div>
+<div className="flex flex-col mt-24 ml-8 sm:flex-row sm:ml-0">
+    <div className="flex flex-col items-center sm:items-start sm:w-56">
+        <img src={ansh} alt="Founder" className="lg:w-full lg:mt-5 lg:ml-2 h-auto border-4 border-slate-300" />
+        <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 text-black border-2 border-cyan-950 text-lg flex items-center lg:ml-3 justify-center sm:justify-start">
             Ansh Singh
-            <a href="https://www.linkedin.com/in/ansh-singh-0a5a01226/" target="_blank" rel="noopener noreferrer">
-              <img src={linkedinBlue} alt="LinkedIn" className="ml-2 w-8 h-8" />
+            <a href="https://www.linkedin.com/in/ansh-singh-0a5a01226/" target="_blank" rel="noopener noreferrer" className="ml-2">
+                <img src={linkedinBlue} alt="LinkedIn" className="w-8 h-8" />
             </a>
-          </motion.div>
-        </div>
-        <div className="ml-5 w-full max-w-full">
-          <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-5 h-auto rounded-lg mr-7 text-black border-2 border-cyan-950 text-xl">
+        </motion.div>
+    </div>
+    <div className="ml-0 mt-5 sm:ml-5 sm:w-full max-w-full">
+        <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-5 h-auto rounded-lg mr-7 text-black border-2 border-cyan-950 text-xl">
             This is a large text box next to the image. You can add more content here as needed.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laborum est ipsa, labore error vel accusamus, obcaecati rerum dolorem sapiente fugit adipisci nam iusto quo cupiditate totam tenetur nostrum impedit. This is a large text box next to the image. You can add more content here as needed.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laborumrem ipsum dolor sit amet obcaecati rerum sapiente fugit adipisci nam iusto quo cupiditate totam tenetur nostrum impedit. This is est ipsa, labore error vel accusamus, obcaecati rerum dolorem sapiente fugit adipisci nam iusto quo cupiditate totam tenetur nostrum impedit. This is a large text box next to the image. You can add more content here as needed.
             Loa large text box next to the image. You can add more content here as needed.
-          </motion.div>
-        </div>
-      </div>
+        </motion.div>
+    </div>
+</div>
+
 
       {/* Core Members section */}
-      <div className="relative right-[33rem] mb-8 mt-16 text-n-5 text-5xl">Core Members</div>
-      <div className="w-full flex flex-row justify-start items-start mt-2 ml-8">
-        <div className="flex flex-col items-center">
-          <motion.img initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} src={siddhant} alt="Siddhant" className="w-56 border-4 h-full border-slate-300 mr-2" />
-          <motion.div initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 mr-2 text-black border-2 border-cyan-950 text-lg flex items-center">
-            Siddhant Singh
-            <a href="https://www.linkedin.com/in/siddhant-kumar-singh/" target="_blank" rel="noopener noreferrer">
-              <img src={linkedinBlue} alt="LinkedIn" className="ml-2 w-8 h-8" />
-            </a>
-          </motion.div>
-        </div>
-        <div className="flex flex-col mb-2 items-center">
-          <motion.img initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} src={suraj} alt="Suraj" className="w-52 h-full mr-2 border-4  border-slate-300" />
-          <motion.div initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="bg-gray-100 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 text-black border-2 border-cyan-950 text-lg flex items-center">
-            Suraj Kumar
-            <a href="https://www.linkedin.com/in/surajkumar06022003/" target="_blank" rel="noopener noreferrer">
-              <img src={linkedinBlue} alt="LinkedIn" className="ml-2 w-8 h-8" />
-            </a>
-          </motion.div>
-          
-        </div>
+      <div className="relative lg:right-[33rem] mb-8 mt-16 text-n-5 text-5xl">Core Members</div>
+      <div class="w-full flex flex-col md:flex-row justify-center md:justify-start items-center md:items-start mt-2 md:ml-8">
+  <div class="flex flex-col  md:mr-8">
+    <motion.img initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} src={siddhant} alt="Siddhant" class="w-60 border-4 mt-3 h-full border-slate-300 mr-2" />
+    <motion.div initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} class="bg-gray-100 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 mr-2 text-black border-2 border-cyan-950 text-lg flex items-center">
+      Siddhant Singh
+      <a href="https://www.linkedin.com/in/siddhant-kumar-singh/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinBlue} alt="LinkedIn" class="ml-2 w-8 h-8" />
+      </a>
+    </motion.div>
+  </div>
+  <div class="flex flex-col items-center mt-2 md:mb-2 md:items-start">
+    <motion.img initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} src={suraj} alt="Suraj" class="w-56 h-full mr-2 border-4  border-slate-300" />
+    <motion.div initial={{ opacity: 0, y: 200 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} class="bg-gray-100 lg:ml-6 backdrop-blur-md bg-opacity-10 p-3 h-auto rounded-lg mt-2 text-black border-2 border-cyan-950 text-lg flex items-center">
+      Suraj Kumar
+      <a href="https://www.linkedin.com/in/surajkumar06022003/" target="_blank" rel="noopener noreferrer">
+        <img src={linkedinBlue} alt="LinkedIn" class="ml-2 w-8 h-8" />
+      </a>
+    </motion.div>
+
+</div>
+
         
-        <div className="flex justify-end w-full ">
+        <div className="flex justify-between flex-wrap py-2 lg:px-8 px-28 ">
           
 
         {/* Image 1 */}
